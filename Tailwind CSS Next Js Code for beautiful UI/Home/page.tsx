22316d4251473d3be4c273d619785818757a3059
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-11-03 16:53:00 +0600

    Update page.tsx

--- Tailwind CSS Next Js Code for beautiful UI/Home/page.tsx
+++ Tailwind CSS Next Js Code for beautiful UI/Home/page.tsx
@@ -1,24 +1,20 @@
 import Image from "next/image";
-//import Frame from "./Frame.png";
 import Frame from "../Frame.png";
 import Img from "./img.png";
 export default function Home() {
   return (
-    // simple validation: valid email
-
     <div className="flex min-h-screen">
       <div className="flex-1 flex flex-col justify-center items-center bg-white px-16">
         <div className="w-full max-w-md">
-          <div className=" mb-8">
+          <div className="mb-8">
             <div>{/*<img src={Img}></img>*/}</div>
             <h1 className="text-4xl font-bold text-black mt-4 ml-2 ">
               Welcome Back <br /> PULSE!
             </h1>
           </div>
-          <form className="flex flex-col gap-6 mt-4">
-            <div className="flex flex-col">
+          <form className="flex flex-col gap-6 mt-4 ml-2">
+            <div className="flex flex-col ">
               <label className="mb-1 text-gray-500">E-mail</label>
-
               <input
                 type="email"
                 placeholder="example@gmail.com"
@@ -43,32 +39,38 @@ export default function Home() {
               </div>
             </div>
             <div className="flex justify-between items-center text-sm">
-              <label className="flex items-center gap-2">
+              <label className="flex items-center gap-2 text-gray-500">
                 <input type="checkbox" />
                 Remember me
               </label>
-              <a href="#" className="text-yellow-500">
+              <a href="#" className="text-yellow-500 hover:underline">
                 Forgot Password?
               </a>
             </div>
-            <button className="bg-yellow-400 text-black font-semibold py-2 rounded-full hover:bg-yellow-500 transition active:bg-green-400 transition">
+            <button className="bg-yellow-400 text-black font-semibold py-2 rounded-full hover:bg-yellow-500 transition active:bg-green-400 transition cursor-pointer">
               Sign in
             </button>
           </form>
         </div>
       </div>
-
-      <div className="flex-1 relative hidden md:flex items-center justify-center bg-gradient-to-b from-yellow-400 to-yellow-100 mt-3 mb-4 mr-4 rounded-md width={947} height={960}">
-        <div className="items-center px-8 py-2">
-          <h2 className="text-3xl font-bold mb-4 pl-5 -mt-2">
+      <div className="flex-1 relative hidden md:flex items-center justify-center bg-gradient-to-br from-yellow-400 to-yellow-100 mt-7 mb-4 mr-7 rounded-md width={947} height={960} rounded-4xl">
+        <div className="items-center px-8 py-2 space-y-6 -translate-y-3">
+          <h2 className="text-3xl font-bold mb-4 pl-5 mt-30">
             The Simplest way to manage <br />
             your workforce.
           </h2>
           <p className="text-gray-700 mb-4 pl-5 -mt-2">
             Enter your credentials to access your account.
           </p>
-
-          {<Image src={Frame} alt="Illustration" width={846} height={401} />}
+          {
+            <Image
+              src={Frame}
+              alt="Illustration"
+              width={947}
+              height={960}
+              className="mt-30"
+            />
+          }
         </div>
       </div>
     </div>
